629d0236d884fcf323714ae92131952d60d4cd33
MalakhovSky <tviler6@gmail.com>
2023-06-05 18:23:33 +0300

    create register

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,11 @@
-const { prisma } = require('@prisma/client').PrismaClient
+const { prisma } = require('../prisma/prisma-client');
 const brypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
-    res.send('login');
+    const {email,password} = req.body;
 
-    if(!email && password){
+    if(!email || !password){
         return res.status(400).json({message:'Пожайлуста, заполните поля'})
     }
     const user = await prisma.user.findFirst({
@@ -24,8 +24,43 @@ const login = async (req, res) => {
         return res.status(400).json({message:'Неверный логин или пароль'})
     }
 }
+
+// @route POST /api/ user/ register
 const register = async (req, res) => {
-    res.send('register');
+    const {email,password,name} = req.body;
+    if(!email || !password || !name) {
+        return res.status(400).json({message:"Пожайлуста заполните обязательные поля"})
+    }
+    const registeredUser = await prisma.user.findFirst({
+        where:{
+            email
+        }
+    })
+    if(registeredUser){
+        return res.status(400).json({message: 'Пользователь, с таким email уже существует '})
+    }
+    const salt = await brypt.genSalt(10);
+    const hashedPassword = await brypt.hash(password,salt)
+
+    const user = await prisma.user.create({
+        data:{
+            email,
+            name,
+            password:hashedPassword,
+        }
+    })
+    const secret = process.env.JWT_SECRET;
+    if(user && secret){
+        res.status(201).json({
+            id:user.id,
+            email: user.email,
+            name:user.name,
+            token: jwt.sign({id:user.id},secret,{expiresIn:'30d'})
+
+        })
+    }else{
+        return res.status(400).json({message:'Не удалось создать пользователя'})
+    }
 }
 const current = async (req, res) => {
     res.send('current');
